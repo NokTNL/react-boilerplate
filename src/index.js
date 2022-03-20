f2965423297f6f8af1674482a7dc1887d159e19b
NokTNL <noklamtsz@gmail.com>
2022-03-20 08:57:55 +0000

    Replace with normalize.css

--- src/index.js
+++ src/index.js
@@ -1,9 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import App from "./App";
-
 import { Provider } from "react-redux";
 import store from "./store/mainStore";
+
+import App from "./App";
+// normalize.css
+import "./index.css";
 
 ReactDOM.render(
   <React.StrictMode>
